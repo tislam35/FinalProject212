740c1bc03d3df9e08280e4070eaa20ba429d3c94
Mohfujul Mohammed <47184815+mohfujulm@users.noreply.github.com>
2019-05-18 15:37:26 -0400

    implemented searchFor function
    searchFor function traverses entire graph recursively and checks if a station with the given target name exists in the graph

--- subwaygraph.h
+++ subwaygraph.h
@@ -32,15 +32,46 @@ namespace graph {
                 return false;
             }
 
-            void setConnection(graph_node* a, graph_node* b) {
-                if (sharedTrain(a,b) == true) {
-                    a->setOut(b);
-                    b->setOut(a);
+            bool compareName(std::string target, graph_node* A) {    
+                if (A->stationName() == target) {
+                    return true;
+                }       
+                else {
+                    return false;
                 }
             }
 
-            //method for traversal
+            void setConnection(graph_node* a, graph_node* b) {
+                if (sharedTrain(a,b) == true) {
+                    a->setOutAdd(b);
+                    b->setOutAdd(a);
+                }
+            }
+
+            std::vector<char> routeTo(std::string start, std::string target) {
+
+            }
             
+            //method for searching to see if a station exists in a subway map            
+
+            bool searchFor(std::string target, subway_graph source) {
+                for (int i=0;i<current->getOut().size();i++) {              //check surrounding stations to see if any matches hit, if so return true and end the recursion
+                    if (compareName(target, current->getOut()[i]) == true) {     
+                        return true;
+                    }
+                }
+                current->setMarked();                                       //since surrounding stations have been checked, this station is marked off so that it is not recurred on (otherwise it would be an endless recursion)
+                for (int i=0;i<current->getOut().size();i++) {
+                    if (current->getOut()[i]->getMarked() == false) {       //checks if the station about to recur on is marked or not to prevent endless recursion using the previous line
+                        subway_graph temp(current->getOut()[i]);
+                        if (searchFor(target, temp) == true) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
             //train to take between stops
             
             //under construction
@@ -53,8 +84,4 @@ namespace graph {
             int length;
 
     };
-    //is 
 }
-//setconnection
-//
-//create link 
