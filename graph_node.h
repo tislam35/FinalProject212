d3ed6cbd4681eee0772446593bdc9c014f404caf
Tohidul Islam <tislam35@gmail.com>
2019-05-18 14:33:48 -0400

    Added several functions that will help traverse/add nodes/delete nodes

--- graph_node.h
+++ graph_node.h
@@ -2,62 +2,64 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 
-namespace final_Proj_212{
+namespace graph{
     class graph_node{
         public:
-            graph_node(std::string a, std::vector<char> b){
+            graph_node(std::string a, std::string b){
                 station = a;
+                marked = false;
                 for(int i = 0; i < b.size(); i++){
                     trains.push_back(b[i]);
                 }
-                marked = false;
             }
-            /**
-            std::vector<graph_node*> getNext(){
-                return next;
-            }
-            std::vector<graph_node*> getPrev(){
-                return prev;
-            }
-            std::vector<graph_node*> getExpNext(){
-                return expNext;
-            }
-            std::vector<graph_node*> getExpPrev(){
-                return expPrev;
-            }*/
+
             std::vector<graph_node*> getOut(){
                 return out;
             }
+
             std::vector<char> getTrains(){
                 return trains;
             }
+
+            bool getMarked(){
+                return marked;
+            }
+
             std::string stationName(){
                 return station;
             }
-            /**
-            void setNext(graph_node* newConnection){
-                next.push_back(newConnection)
+
+            //this will return the position of the target station in the vector of connected
+            //stations or return -1 if the target station is not connected with the current one
+            int getPos(graph_node* target){
+                for(int i = 0; i < out.size(); i++){
+                    if(out[i] == target)
+                        return i;
+                }
+                return -1;
             }
-            void setPrev(graph_node* newConnection){
-                prev.push_back(newConnection)
+
+            //adds a connection between 2 stations
+            void setOutAdd(graph_node* newConnection){
+                out.push_back(newConnection);
             }
-            void setExpNext(graph_node* newConnection){
-                expNext.push_back(newConnection)
+
+            //deletes a connection between 2 stations
+            void setOutDelete(int pos){
+                out.erase(out.begin()+(pos-1));
             }
-            void setExpPrev(graph_node* newConnection){
-                expPrev.push_back(newConnection)
-            }*/
-            void setOut(graph_node* newConnection){
-                out.push_back(newConnection)
+
+            //resets marked to false
+            void resetMarked(){
+                marked = false;
             }
+
         private:
             std::vector<graph_node*> out;
-            //std::vector<graph_node*> prev;
-            //std::vector<graph_node*> expNext;
-            //std::vector<graph_node*> expPrev;
             std::string station;
             std::vector<char> trains;
             bool marked;
-    }
+    };
 }
